1183f3adec46ab8cf50f5eccffad094fd8f9c121
GG <rkrudtls@gmail.com>
2022-08-08 14:56:23 +0900

    .

---
+++ src/main/java/com/codestates/practicegithubActionpractice/PracticeGithubActionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActionpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithubActionpractice/PracticeGithubActionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
